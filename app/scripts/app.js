f347925a445d49005cb80a07b96fe743b0bf01a3
Mila <llysenko23@rambler.ru>
2016-11-06 14:59:01 +0200

    Start new project.

---
+++ app/scripts/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2'
+
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+  $routeProvider.
+  when('/view1', {
+    templateUrl: 'app/views/view1.html'
+  }).
+  when('/view2', {
+    templateUrl: 'app/views/view2.html'
+  }).
+  otherwise('app/views/view1.html');
+
+  /*$routeProvider.otherwise({redirectTo: 'app/views/view1'});*/
+}]);
